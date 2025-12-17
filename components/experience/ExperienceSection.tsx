001324be05def071efa660b213a29195a8c9cf6a
Russell <rssliu88@gmail.com>
2025-12-16 21:55:58 -0500

    experience card + modal

--- components/experience/ExperienceSection.tsx
+++ components/experience/ExperienceSection.tsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
-import Modal from "../components/BaseModal";
 import Section from "../components/Section";
 import { useNavContext } from "../context/NavContext";
 import { experience } from "../lib/experience";
 import ExperienceCard from "./ExperienceCard";
-import { Experience } from "../lib/types";
-import ExperienceModal from "./ExperienceModal";
 const ExperienceSection = () => {
   const context = useNavContext();
-  const [activeExperience, setActiveExperience] = useState<Experience | null>(
-    null,
-  );
+
   return (
     <Section sectionName="experience" onEnter={context.setCurrentSection}>
       <div className="experience-list">
         {experience.map((exp) => (
           <ExperienceCard
+            key={exp.company+exp.startMonth+exp.startYear}
             experience={exp}
-            onClick={() => {
-              setActiveExperience(exp);
-            }}
           />
         ))}
       </div>
-      {activeExperience && 
-      <ExperienceModal 
-      experience={activeExperience} 
-      onClick={()=>setActiveExperience(null)}
-      
-      />}
+
     </Section>
   );
 };
