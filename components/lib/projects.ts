1bc908fa3efa2d83f0d0f15c5c7ca07da81c1d5c
Russell <rssliu88@gmail.com>
2025-12-19 16:13:03 -0500

    update project desc

--- components/lib/projects.ts
+++ components/lib/projects.ts
@@ -4,16 +4,16 @@ export const projects: Project[] = [
   {
     name: "MiniTS",
     description:
-      "TypeScript-inspired language supporting classes, control flow, lists, and static type checking. Additionally, lexical scoping, variable shadowing, and AST-based semantic analysis.",
+      "TypeScript-inspired language with an online IDE supporting classes, control flow, lists, and static type checking. Additionally, lexical scoping, variable shadowing, and AST-based semantic analysis.",
     websiteUrl: "",
-    technologies: ["Java", "ANTLR"],
+    technologies: ["Java", "Spring Boot","TypeScript","React", "ANTLR"],
   },
   {
     name: "Untitled",
     description:
       "Workout tracker with streak visualizations, fine-tuned LLM (OpenAI) feedback, storage + auth, and caching/rate-limiting.",
     websiteUrl: "",
-    technologies: ["Next.js", "TypeScript", "MongoDB", "Redis", "S3"],
+    technologies: ["Next.js", "TypeScript", "Python","MongoDB", "Redis", "S3"],
   },
 
   {
@@ -34,12 +34,12 @@ export const projects: Project[] = [
     description:
       "Blog site supporting authenticated public/private post creation, as well as liking and commenting.",
     websiteUrl: "",
-    technologies: ["React", "Express", "MongoDB"],
+    technologies: ["JavaScript","React", "Express", "MongoDB"],
   },
   {
     name: "YorkRMC",
     description:
-      "A Chrome extension that scrapes RateMyCourses.io and displays course reviews directly on university course page.",
+      "A Chrome extension that scrapes RateMyCourses.io and displays course reviews directly on my university's course page.",
     websiteUrl: "",
     technologies: ["JavaScript", "Express"],
   },
