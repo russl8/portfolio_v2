8c14f5762fc27d0791e50c63563b1c3ab78d4fac
Russell <rssliu88@gmail.com>
2025-12-24 23:26:08 -0500

    project rewording

--- components/lib/projects.ts
+++ components/lib/projects.ts
@@ -4,14 +4,14 @@ export const projects: Project[] = [
   {
     name: "MiniTS",
     description:
-      "TypeScript-inspired language with an online IDE supporting classes, control flow, lists, and static type checking. Additionally, lexical scoping, variable shadowing, and AST-based semantic analysis.",
+      "TypeScript-inspired language with an online IDE supporting basic inheritance, control flow, lists, static type-checking, scoping, variable shadowing. Uses ANTLR for AST-based semantic & syntactic analysis.",
     websiteUrl: "https://minits-ui.vercel.app/",
     technologies: ["Java", "Spring Boot","TypeScript","React", "ANTLR"],
   },
   {
     name: "Workout Tracker",
     description:
-      "Featuring streak graph visualizations, fine-tuned LLM (OpenAI) feedback, storage + auth, and caching/rate-limiting.",
+      "Personal workout tracker with streak visualizations, fine-tuned LLM (OpenAI) feedback, and caching/rate-limiting.",
     websiteUrl: "https://github.com/russl8/untitled/",
     technologies: ["Next.js", "TypeScript", "Python","MongoDB", "Redis", "S3"],
   },
@@ -19,7 +19,7 @@ export const projects: Project[] = [
   {
     name: "Video Processing Service",
     description:
-      "Cloud-native video upload/playback service leveraging Pub/Sub and Cloud Run to trigger automated 360p video transcoding.",
+      "Cloud-native video upload/playback service using Pub/Sub and Cloud Run to trigger an automated video transcoding (to 360p) task.",
     websiteUrl: "https://github.com/russl8/vid-player",
     technologies: [
       "TypeScript",
@@ -32,14 +32,14 @@ export const projects: Project[] = [
   {
     name: "BeGrate",
     description:
-      "Blog site supporting authenticated public/private post creation, as well as liking and commenting.",
+      "Blog site supporting authenticated public/private post creation + liking and commenting.",
     websiteUrl: "https://github.com/russl8/BeGrate",
     technologies: ["JavaScript","React", "Express", "MongoDB"],
   },
   {
     name: "YorkRMC",
     description:
-      "A Chrome extension that scrapes RateMyCourses.io and displays course reviews directly on my university's course page.",
+      "Chrome extension to scrape RateMyCourses.io for course reviews and displays them on my university's respective course page.",
     websiteUrl: "https://chromewebstore.google.com/detail/yorkrmc/idmpighcjfjokifedbmlnmlfdjedidfb",
     technologies: ["JavaScript", "Express"],
   },
